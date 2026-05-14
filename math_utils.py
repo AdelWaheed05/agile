04fd0ab62081762a6c49e40c59ee7e3ca30d4eb1
AdelWaheed05 <adelwaheedd@gmail.com>
2026-05-15 02:55:01 +0300

    Add files via upload

---
+++ math_utils.py
@@ -0,0 +1,5 @@
+def add(a, b):
+    return a + b [cite: 8, 9]
+
+def subtract(a, b):
+    return a - b [cite: 10, 11]
---
+++ scripts/test_math_utils.py
@@ -0,0 +1,7 @@
+from math_utils import add, subtract [cite: 14]
+
+def test_add():
+    assert add(2, 3) == 5 [cite: 15, 16]
+
+def test_subtract():
+    assert subtract(5, 3) == 2 [cite: 17, 18]
